41fcf717f1c7bf36a4c3ef6c690baa042335584b
Evert Pot <evert@rooftopsolutions.nl>
2012-02-08 19:01:25 +0100

    New text by mr. willem

--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,27 @@
 <h2 class="contentheading">
 	Meldpunt Noord, Midden, Oost, West en Zuid-Nederlanders</h2>
 
+<p>
+Sinds 22 februari 2006 is er sprake van hufterigheid, hypocrisie en onbenul in de Nederlandse politiek. 
+Op dit moment lopen de schattingen naar het aantal mensen binnen Nederland die de politiek te kijken 
+zetten uiteen van 5 tot 39 procent van de stemgerechtigden, met een groot aantal aanhangers en 
+sympathisanten eromheen.
+</p>
 
-<p>Sinds 22 februari 2006 is er sprake van hufterigheid en onbenul in de Nederlandse politiek. Op dit moment lopen de schattingen naar het aantal mensen uit deze landen, dat in Nederland de politiek te kijken zet, uiteen van 24 tot 38 personen. </p>
+<p>
+Deze massale politieke stupiditeit en verering van onderbuikgevoelens leidt tot veel problemen; 
+overlast, belediging, grove generalisaties, respectloosheid en integratie- en 
+medemenselijkheidsproblemen. Voor veel mensen vormen deze zaken een serieus probleem. Klachten worden 
+echter vaak niet gemeld, omdat men het idee heeft dat er toch niets mee wordt gedaan. Of uit angst voor
+verbale of fysieke agressie.
+</p>
 
-<p>Deze massale arbeidsimmigratie leidt tot veel problemen; overlast, vervuiling, verdringing op de arbeidsmarkt en integratie- en huisvestingsproblemen. Voor veel mensen vormen deze zaken een serieus probleem. Klachten worden echter vaak niet gemeld, omdat men het idee heeft dat er toch niets mee wordt gedaan.</p>
-
-<p>Heeft u overlast van MOE-landers? Of bent u uw baan kwijtgeraakt aan een Pool, Bulgaar, Roemeen of andere Midden- of Oost Europeaan? Wij willen het graag horen. De Partij voor de Vrijheid biedt u op deze website een platform om uw klachten te melden. Deze klachten zullen wij in kaart brengen en de resultaten daarvan aanbieden aan de minister van Sociale Zaken en Werkgelegenheid.</p>
+<p>
+Heeft u overlast van NMOWZ-landers? Of bent u uw liefde voor het vaderland kwijtgeraakt door toedoen
+van deze hypocriete en kortzichtige Nederlanders? Wij willen het graag horen. De Partij voor de
+Blijheid biedt u op deze website een platform om uw klachten te melden. Deze klachten zullen wij in
+kaart brengen en de resultaten daarvan aanbieden aan de minister van Sociale Zaken en Werkgelegenheid.
+Of niet, we zien wel.
+</p>
 
 <h1 style="text-align: right;"><a href="?page=melden">Meld uw verhaal hier</a></h1>
