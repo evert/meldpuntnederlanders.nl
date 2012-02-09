cedbf97c18cbb618fdf46cee2e4cd04fa0dec16f
Evert Pot <evert@rooftopsolutions.nl>
2012-02-09 12:02:28 +0100

    Willems text

--- pages/home.php
+++ pages/home.php
@@ -1,27 +1,34 @@
 <h2 class="contentheading">
-	Meldpunt Noord, Midden, Oost, West en Zuid-Nederlanders</h2>
+    Meldpunt Noord, Midden, Oost, West en Zuid-Nederlanders</h2>
 
-<p>
-Sinds 22 februari 2006 is er sprake van hufterigheid, hypocrisie en onbenul in de Nederlandse politiek. 
-Op dit moment lopen de schattingen naar het aantal mensen binnen Nederland die de politiek te kijken 
-zetten uiteen van 5 tot 39 procent van de stemgerechtigden, met een groot aantal aanhangers en 
-sympathisanten eromheen.
-</p>
 
-<p>
-Deze massale politieke stupiditeit en verering van onderbuikgevoelens leidt tot veel problemen; 
-overlast, belediging, grove generalisaties, respectloosheid en integratie- en 
-medemenselijkheidsproblemen. Voor veel mensen vormen deze zaken een serieus probleem. Klachten worden 
-echter vaak niet gemeld, omdat men het idee heeft dat er toch niets mee wordt gedaan. Of uit angst voor
-verbale of fysieke agressie.
-</p>
+<p>Sinds 22 februari 2006 is er sprake van een vrijbrief voor hufterigheid,
+hypocrisie en verbaal geweld in de Nederlandse politiek. Feiten en logica maken
+plaats voor chauvinisme en opruiende taal. Want Nederland heeft een unieke
+cultuur en die moet beschermd worden. Zelfs als je daarvoor de redelijkheid en
+het fatsoen moet overschrijden.</p>
 
-<p>
-Heeft u overlast van NMOWZ-landers? Of bent u uw liefde voor het vaderland kwijtgeraakt door toedoen
-van deze hypocriete en kortzichtige Nederlanders? Wij willen het graag horen. De Partij voor de
-Blijheid biedt u op deze website een platform om uw klachten te melden. Deze klachten zullen wij in
-kaart brengen en de resultaten daarvan aanbieden aan de minister van Sociale Zaken en Werkgelegenheid.
-Of niet, we zien wel.
-</p>
+
+<p>Dus wordt u beschuldigd van het stelen van banen van de mensen die zichzelf
+eigenlijk te goed voelen om ze te doen. Werk waar je de handen voor vuil moet
+maken, dat is ten slotte ook te min voor zelfs de meest uitkeringsgerechtigde
+Nederlander. Maar dat doet er niet toe, het betreft tenslotte buitenlanders. En
+dat is een tot overlast, criminaliteit en dronkenschap neigend irritant volkje
+die buitenlanders. Nou ja, buitenlanders met een slechtere economie dan die van
+ons dan hé? Althans, zo ziet de NMOWZ-lander het.</p>
+
+<p>De verering van onderbuikgevoelens en angst voor het vreemde leidt tot veel
+problemen; overlast, belediging, grove generalisaties, respectloosheid en
+integratie- en medemenselijkheidsproblemen. Voor veel mensen vormen deze zaken
+een serieus probleem. Klachten worden echter vaak niet gemeld, omdat men het
+idee heeft dat er toch niets mee wordt gedaan. Of uit angst voor verbale of
+fysieke agressie. En dat is ons een doorn in het oog.</p>
+
+<p>Heeft u overlast van NMOWZ-landers? Of bent u uw liefde voor het vaderland
+kwijtgeraakt door toedoen van deze hypocriete en kortzichtige Nederlanders? Wij
+willen het graag horen. De Partij voor de Blijheid biedt u op deze website een
+platform om uw klachten te melden. Deze klachten zullen wij in kaart brengen en
+de resultaten daarvan aanbieden aan de minister van Sociale Zaken en
+Werkgelegenheid. Of niet, we zien wel.</p>
 
 <h1 style="text-align: right;"><a href="?page=melden">Meld uw verhaal hier</a></h1>
