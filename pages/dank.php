9eda0c74905ad806c99e0a965a7dad2dc342ec5e
003Labs <003Labs@gmail.com>
2012-02-10 19:36:34 +0100

    added like box

--- pages/dank.php
+++ pages/dank.php
@@ -3,3 +3,7 @@
 <p>Heel erg bedankt voor het invullen van dit formulier. Uw melding draagt enorm bij aan het in kaart brengen van meldingen over hufters binnen nederland.</p>
 
 <p><a href="?page=about">Over ons</a></p>
+
+<hr />
+
+<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FMeldpuntNL&amp;width=400&amp;height=290&amp;colorscheme=light&amp;show_faces=true&amp;border_color&amp;stream=false&amp;header=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:400px; height:290px;" allowTransparency="true"></iframe>
